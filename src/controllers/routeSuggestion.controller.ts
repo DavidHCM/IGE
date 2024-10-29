02b79c91dac63c66205fd691d5436afccdfc6d1c
David Hernández <97574531+DavidHCM@users.noreply.github.com>
2024-10-28 19:07:12 -0600

    Agregando las rutas interactivas con mapa

--- src/controllers/routeSuggestion.controller.ts
+++ src/controllers/routeSuggestion.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
 import RouteSuggestion from './../models/routeSuggestion.model';
 import { HTTP_STATUS } from '../types/http-status-codes';
 import { RouteSuggestion as RouteType } from "../types/routeSuggestion";
@@ -107,6 +108,32 @@ class routeSuggestionController {
             res.status(status).send({ message, error: err });
         }
     }
+
+    async routeFromMap(req: Request, res: Response): Promise<void> {
+        try {
+            const { start, end } = req.body;
+            console.log(start, end);
+
+            if (!start || !end) {
+                throw ('Start and end cordinated requeried: ' + HTTP_STATUS.CONFLICT);
+            }
+
+            // TODO: Esta url del server cambiaria en produccion
+            const osrmUrl = `http://localhost:5000/route/v1/driving/${start[0]},${start[1]};${end[0]},${end[1]}?overview=full&geometries=geojson`;
+            const response = await axios.get(osrmUrl);
+
+            const routeData = response.data;
+
+            // Enviar la respuesta al cliente en formato GeoJSON
+            res.status(200).json(routeData);
+        } catch (err) {
+            const status = err instanceof Error && 'status' in err ? (err as any).status : HTTP_STATUS.BAD_REQUEST;
+            const message = err instanceof Error && 'message' in err ? err.message : 'Error deleting route suggestion';
+
+            res.status(status).send({ message, error: err });
+        }
+    }
+
 }
 
 export const routeSuggestionControllers = new routeSuggestionController();
